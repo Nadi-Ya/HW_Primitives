3cac9a1bcb3c3cec0ba2f38b5ababe422e37e4ba
Nadya <nadezhde@inbox.ru>
2024-08-13 19:17:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int rublesPerMile = 20;
+        int bonusMiles = ticketPrice / rublesPerMile;
+        System.out.println("Bonus Miles: " + bonusMiles);
+    }
+}
